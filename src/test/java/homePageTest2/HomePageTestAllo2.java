3012ef6440b5e6a69ca27bb59111c44330b9a184
maverik7189 <maverik7189@gmail.com>
2025-04-23 19:09:20 +0300

    My Test2 ..

---
+++ src/test/java/homePageTest2/HomePageTestAllo2.java
@@ -0,0 +1,40 @@
+package homePageTest2;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class HomePageTestAllo2 {
+
+        private WebDriver driver;
+
+        @Before
+        public void setUp() {
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        }
+
+        @Test
+        public void testAirPods3Search() throws InterruptedException {
+
+            driver.get("https://allo.ua/");
+
+            try {
+                WebElement element = driver.findElement(By.xpath("//*[@id='__layout']/div/header/div[1]/div[2]/a/img")); // можна By.id, By.xpath тощо
+                System.out.println("Елемент знайдено!");
+            } catch (NoSuchElementException e) {
+                System.out.println("Елемент не знайдено.");
+            }
+
+
+            driver.quit();
+
+        }
+}
